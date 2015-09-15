62e975ddacc9ceacc03c7a671142875a9d2dcd3d
dimalosos <kozmintranslate@gmail.com>
2015-09-15 17:40:00 +0300

    jinja2 templates & loop index added

--- my_site/main.py
+++ my_site/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, request
-
+from jinja2 import Template
 app = Flask(__name__, template_folder='.')
 
 @app.route('/')
@@ -8,7 +8,16 @@ def home():
 
 @app.route('/users', methods = ['GET'])
 def users():
-		return render_template('users.html')
+		user = {
+			'name': 'John',
+			'email': 'johnny@gmail.com',
+		}
+		return render_template(
+			'users.html', 
+			title='Users',
+			user=user,
+			view_data={'user': user, 'user_friends': ['Bob', 'Alice']}
+		)
 
 
 if __name__ == '__main__':
